c3e7cdd21c4a966333fc70247be7ed99bea5d323
yeheng.ma <Myh_00@outlook.com>
2024-04-25 19:23:37 +1200

    delete msg schema

--- onlinechat-server/src/db/schema.js
+++ onlinechat-server/src/db/schema.js
@@ -12,25 +12,5 @@ const userSchema = new Schema({
   },
 });
 
-const msgSchema = new Schema({
-  content: {
-    type: String,
-    required: true,
-  },
-  senderID: {
-    type: String,
-    required: true,
-  },
-  receiverID: {
-    type: String,
-    required: true,
-  },
-  sendTime: {
-    type: String,
-    required: true,
-  },
-});
-
 const User = mongoose.model("User", userSchema);
-const Msg = mongoose.model("Msg", msgSchema);
-export { User, Msg };
+export { User };
